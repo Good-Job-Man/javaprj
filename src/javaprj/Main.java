782b3fc68b53372ded81f9e12d2db6ab15aa6537
Good-Job-Man <h50521478@gmail.com>
2026-02-04 12:17:43 +0900

    Update Main.java

--- src/javaprj/Main.java
+++ src/javaprj/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("개발자 1이 개발했습니다");
 		System.out.println("개발자 2가 개발했습니다");
+		System.out.println("개발자 2.1가 개발했습니다");
+			
 	}
 
 }
